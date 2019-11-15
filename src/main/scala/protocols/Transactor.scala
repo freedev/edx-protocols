35265a72c0f725f0dca434f85fd79c58fc29be5c
Vincenzo D'Amore <v.damore@gmail.com>
2019-11-15 14:52:58 +0100

    great!

--- src/main/scala/protocols/Transactor.scala
+++ src/main/scala/protocols/Transactor.scala
@@ -18,7 +18,7 @@ object Transactor {
 
     sealed trait Command[T] extends PrivateCommand[T]
     final case class Begin[T](replyTo: ActorRef[ActorRef[Session[T]]]) extends Command[T]
-    final case class Timeout[T](replyTo: ActorRef[ActorRef[Session[T]]]) extends Command[T]
+    final case class Timeout[T](replyTo: ActorRef[Session[T]]) extends Command[T]
 
     sealed trait Session[T] extends Product with Serializable
     final case class Extract[T, U](f: T => U, replyTo: ActorRef[U]) extends Session[T]
@@ -43,16 +43,11 @@ object Transactor {
                     println(s"commitedBuilder RolledBack!!! $value")
 //                    session.narrow.tell(Stopped[T]())
                     ctx.stop(session)
-                    Behaviors.empty[PrivateCommand[T]]
+                    idle(value, sessionTimeout)
                 }
                 case (ctx, Begin(replyTo)) => {
                     println("Begin!!!")
-                    val sh = sessionHandler(value, ctx.self, Set(), sessionTimeout)
-                    val actorRef = ctx.spawnAnonymous(sh)
-                    ctx.watch(actorRef)
-
-                    replyTo ! actorRef
-                    ctx.scheduleOnce(sessionTimeout, ctx.self, RolledBack(actorRef))
+                    handleBegin(value, sessionTimeout, ctx, replyTo)
 
                     Behaviors.same[PrivateCommand[T]]
                 }
@@ -61,10 +56,19 @@ object Transactor {
                 }
                 case (ctx, _) => {
                     println("Others!!!")
-                    idle[T](value, sessionTimeout)
+//                    idle[T](value, sessionTimeout)
                     Behaviors.same[PrivateCommand[T]]
                 }
             }
+    }
+
+    private def handleBegin[T](value: T, sessionTimeout: FiniteDuration, ctx: ActorContext[PrivateCommand[T]], replyTo: ActorRef[ActorRef[Session[T]]]) = {
+        val sh = sessionHandler(value, ctx.self, Set(), sessionTimeout)
+        val actorRef = ctx.spawnAnonymous(sh)
+        ctx.watch(actorRef)
+
+        replyTo ! actorRef
+        ctx.scheduleOnce(sessionTimeout, ctx.self, Timeout(actorRef))
     }
 
     /**
@@ -100,15 +104,31 @@ object Transactor {
       *   - Messages other than [[Begin]] should not change the behavior.
       */
     private def idle[T](value: T, sessionTimeout: FiniteDuration): Behavior[PrivateCommand[T]] = {
+        println("Entering idle value:" + value + " sessionTimeout:" + sessionTimeout)
         Behaviors.receive[PrivateCommand[T]] {
-            case (ctx, Committed(session, value)) => {
-                Behaviors.same[PrivateCommand[T]]
+            case (ctx, Committed(session, newValue)) => {
+                println("idle Committed!!!")
+                //                    session ! Stopped[T]()
+                parentBuilder(newValue, sessionTimeout)
             }
             case (ctx, RolledBack(session)) => {
+                println(s"idle RolledBack!!! $value")
+                //                    session.narrow.tell(Stopped[T]())
+                ctx.stop(session)
+                Behaviors.empty[PrivateCommand[T]]
+            }
+            case (ctx, Begin(replyTo)) => {
+                println("idle Begin!!!")
+                handleBegin(value, sessionTimeout, ctx, replyTo)
                 Behaviors.same[PrivateCommand[T]]
             }
+            case (ctx, Timeout(replyTo)) => {
+                println("idle Timeout!!!")
+                Behaviors.stopped[PrivateCommand[T]]
+            }
             case (ctx, _) => {
-                ctx.log.info("idle: message.... ")
+                println("idle Others!!!")
+//                idle[T](value, sessionTimeout)
                 Behaviors.same[PrivateCommand[T]]
             }
         }
